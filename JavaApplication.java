d29e910ff3797829f7d076185135c9df31e896c4
varshap walia <varshwalia@gmail.com>
2017-02-23 12:17:28 +0530

    Add files via upload
    This file inputs Huge Numbers and Calculate the cases of strings 0,1 combination without contiguous 0's. for e.g. enter 3 {111,110,101,011,010} and so on.

---
+++ JavaApplication.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class JavaApplication {
+  private static long naive_calc_fib(int n) {
+    if (n <= 1)
+      return n;
+
+    return naive_calc_fib(n - 1) + naive_calc_fib(n - 2);
+  }
+  
+  private static BigInteger fib_fast(int n){
+	  BigInteger r1, r2, res;
+          
+	  res = new BigInteger("0");
+	  r1=new BigInteger("0");
+	  r2=new BigInteger("1");
+	   for(int i=2;i<=n+2;i++){
+		   res= r1.add(r2);
+		   r1 = r2;
+		   r2 = res;
+	   }
+	   return res;
+  }
+
+  public static void main(String args[]) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Please enter Number of test Cases");
+    int n = in.nextInt();
+       
+	System.out.println(fib_fast(n));
+  }
+}
